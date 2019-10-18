8877310625a9a861a1332b3d101167adad8c3c94
Walderlan Sena <senawalderlan@gmail.com>
2019-10-18 15:47:32 -0300

    Added new layouts.

--- src/Mapping.php
+++ src/Mapping.php
@@ -14,19 +14,25 @@ class Mapping
 {
     private $file;
     private $server;
+    private $xml;
+    private $namespaceMapping;
+    private $nameFileMapping;
 
     public function __construct()
     {
-        $this->file = new File();    
+        $this->file = new File();
+        $this->loadPartFile();
     }
 
     public function create($server)
     {
         $this->server = $server;
+        $this->namespaceMapping = $this->server['argv'][1];
+        $this->nameFileMapping  = $this->server['argv'][2];
 
         $this->argumentsIsValid($this->server);
         
-        $document = $this->file->openFile($this->server['argv'][2]);
+        $document = $this->file->openFile($this->server['argv'][2].'.php');
         
         fwrite($document, "<?php\n");
         
@@ -43,13 +49,14 @@ class Mapping
         
         $this->addAtributes($document);
 
-        $this->file->closeFile($document);
     }
     
     private function addAtributes($document)
     {
         $option = 'y';
-        $data = [];
+        $names = [];
+        $types = [];
+
         do {
             fwrite(STDOUT, 'Enter the field name: ');
             $nameField = $this->readLine();
@@ -60,15 +67,35 @@ class Mapping
                 fwrite(STDOUT,'The type'.$typeField.' not permission !'.PHP_EOL);
                 continue;
             }
-            
-            $data[$nameField][$typeField];
+
+            array_push($names, $nameField);
+            array_push($types, $typeField);
 
             $addType  =  "\tprivate $" . $nameField . ";" ."\n\n";
             fwrite($document, $addType);
             fwrite(STDOUT, 'Do you want to enter another field ? [Y/n] ');
             $option = $this->readLine();
         } while (strtolower($option) == 'y');
+
         fwrite($document, "\n}");
+        
+        $this->file->closeFile($document);
+
+        $this->generateXml(array_combine($names, $types));
+    }
+
+    private function generateXml(array $data)
+    {
+        $nameXml = $this->nameFileMapping.'.mongodb.xml';
+
+        $fileXml = $this->file->openFile($nameXml);
+        fwrite($fileXml, str_replace(['{namespace}', '{collection}'], [$this->namespaceMapping, $this->nameFileMapping], $this->xml['header']));
+        
+        foreach ($data as $key => $value) {
+            fwrite($fileXml, "\t".str_replace(['{name}', '{type}'], [$key, $value], $this->xml['field']).PHP_EOL);
+        }
+        fwrite($fileXml, $this->xml['footer']);
+        $this->file->closeFile($fileXml);
     }
 
     private function readLine()
@@ -113,5 +140,14 @@ class Mapping
         if ($server['argc'] < 3) {
             Message::error('Usage: php dmm.phar NAMESPACE NAME_FILE');
         }
+    }
+
+    private function loadPartFile()
+    {
+        $this->xml = [
+            'header' => file_get_contents(__DIR__.'/layouts/xml/header.template'),
+            'field'  => file_get_contents(__DIR__.'/layouts/xml/field.template'),
+            'footer' => file_get_contents(__DIR__.'/layouts/xml/footer.template'),
+        ];
     }
 }
